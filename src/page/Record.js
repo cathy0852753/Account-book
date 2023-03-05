86d4c8c86c7c5e8b97d1403232b1f081217bfac4
Cathy <cathy0852753@gmail.com>
2023-03-05 19:01:22 +0800

    修改className名稱

--- src/page/Record.js
+++ src/page/Record.js
@@ -5,7 +5,7 @@ import 'semantic-ui-css/semantic.min.css'
 import { AddBtn } from '../components/BodyNav'
 import { Form, Table, } from "react-bootstrap"
 import '../components/css/BodyNav.css'
-import '../components/css/BodyRecord.css'
+import '../components/css/RecordTable.css'
 
 const tableHead = [
   { id: 1, item: null, sort: '分類', way: '方式', account: '帳戶', description: '描述', tag: '標籤', date: '日期', expense: '金額', },
@@ -13,18 +13,18 @@ const tableHead = [
 const tableBody = [
   { id: 1, item: 0, transfer: false, sort: '薪資', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-01-19', expense: 1000, },
   { id: 2, item: 1, transfer: true, sort: '美容美髮', way: '信用卡', account: '兆豐銀行', description: null, tag: '昂貴染護髮', date: '2023-02-19', expense: -1000, },
-  { id: 3, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-03-19', expense: 1000, },
+  { id: 3, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '永豐銀行', description: null, tag: '少少薪資', date: '2023-03-19', expense: 1000, },
   { id: 4, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-04-19', expense: 1000, },
   { id: 5, item: 1, transfer: true, sort: '電子票證加值', way: '轉帳', account: '台新銀行', description: null, tag: '悠遊卡', date: '2023-03-19', expense: -1000, },
   { id: 6, item: 1, transfer: true, sort: '美容美髮', way: '信用卡', account: '兆豐銀行', description: null, tag: '昂貴染護髮', date: '2023-02-19', expense: -1000, },
-  { id: 7, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-03-19', expense: 1000, },
+  { id: 7, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '第一銀行', description: null, tag: '少少薪資', date: '2023-03-19', expense: 1000, },
   { id: 8, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-04-19', expense: 1000, },
   { id: 9, item: 1, transfer: true, sort: '電子票證加值', way: '轉帳', account: '台新銀行', description: null, tag: '悠遊卡', date: '2023-05-19', expense: -1000, },
   { id: 10, item: 1, transfer: true, sort: '電子票證加值', way: '轉帳', account: '台新銀行', description: null, tag: '悠遊卡', date: '2023-05-19', expense: -1000, },
   { id: 11, item: 1, transfer: true, sort: '美容美髮', way: '信用卡', account: '兆豐銀行', description: null, tag: '昂貴染護髮', date: '2023-02-19', expense: -1000, },
   { id: 12, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-03-19', expense: 1000, },
-  { id: 13, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '台新銀行', description: null, tag: '少少薪資', date: '2023-04-19', expense: 1000, },
-  { id: 14, item: 1, transfer: true, sort: '電子票證加值', way: '轉帳', account: '台新銀行', description: null, tag: '悠遊卡', date: '2023-05-19', expense: -1000, },
+  { id: 13, item: 0, transfer: false, sort: '現金', way: '轉帳', account: '華南銀行', description: null, tag: '少少薪資', date: '2023-04-19', expense: 1000, },
+  { id: 14, item: 1, transfer: true, sort: '電子票證加值', way: '轉帳', account: '王道銀行', description: null, tag: '悠遊卡', date: '2023-05-19', expense: -1000, },
 ]
 // Month Select
 let a = [] // a 篩選月份暫存的陣列
@@ -48,7 +48,7 @@ class SelectMonth extends React.Component {
   }
   render () {
     //月份選單預設
-    let defaultM = this.state.month.toString
+    let defaultM
     if (this.state.month < 10) {
       defaultM = '0' + this.state.month
     } else {
@@ -87,10 +87,10 @@ class SelectMonth extends React.Component {
 
     return (
       <>
-        <div className='container-fluid'>
-          <div className='row itemHome'>
+        <div className='record-container-fluid'>
+          <div className='row record-itemHome'>
             <Form.Select
-              className='select yearSelect'
+              className='record-select record-yearSelect'
               onChange={this.changeYear}
               defaultValue={this.state.years}
             >
@@ -98,9 +98,9 @@ class SelectMonth extends React.Component {
                 <option key={el.value} id={el.value}>{el.label}</option>
               )}
             </Form.Select>
-            <span className='dateSpan'>-</span>
+            <span className='record-dateSpan'>-</span>
             <Form.Select
-              className='select monthSelect'
+              className='record-select record-monthSelect'
               onChange={this.changeMonth}
               defaultValue={defaultM}
             >
@@ -119,9 +119,9 @@ class SelectMonth extends React.Component {
 
 const item = (type) => {
   if (type === 1) {
-    return (<label type='button' className="ieItem expensesItem">支</label>)
+    return (<label type='button' className="record-ieItem record-expensesItem">支</label>)
   } else if (type === 0) {
-    return (<label type='button' className="ieItem incomeItem">收</label>)
+    return (<label type='button' className="record-ieItem record-incomeItem">收</label>)
   } else {
     return ('')
   }
@@ -135,43 +135,43 @@ function BodyRecord () {
   let count = a.map(el => el.expense).reduce((a, b) => a + b)
   return (
     <>
-      <div className="bg-record">
-        <Table hover responsive="sm" className="table" >
+      <div className="record-bg-table">
+        <Table hover responsive="sm" className="record-table" >
           <thead>
             {tableHead.map(thead =>
               <tr key={thead.id}>
-                <th className="itemCol" >{thead.item}</th>
-                <th className="sortCol">{thead.sort}</th>
-                <th className="wayCol">{thead.way}</th>
-                <th className="accountCol">{thead.account}</th>
-                <th className="descriptionCol">{thead.description}</th>
-                <th className="tagCol">{thead.tag}</th>
-                <th className="dateCol">{thead.date}</th>
-                <th className="amountCol">{thead.expense}</th>
+                <th className="record-itemCol" >{thead.item}</th>
+                <th className="record-sortCol">{thead.sort}</th>
+                <th className="record-wayCol">{thead.way}</th>
+                <th className="record-accountCol">{thead.account}</th>
+                <th className="record-descriptionCol">{thead.description}</th>
+                <th className="record-tagCol">{thead.tag}</th>
+                <th className="record-dateCol">{thead.date}</th>
+                <th className="record-amountCol">{thead.expense}</th>
               </tr>
             )}
           </thead>
           <tbody>
             {a.map(tbody =>
-              <tr key={tbody.id} className={tbody.transfer ? "trLine trLine-transfer" : "trLine"}>
-                <td className="itemCol">{item(tbody.item)}</td>
-                <td className="sortCol">{tbody.sort}</td>
-                <td className="wayCol">{tbody.way}</td>
-                <td className="accountCol">{tbody.account}</td>
-                <td className="descriptionCol">{tbody.description}</td>
-                <td className="tagCol">{tbody.tag === null ? '' : <label className="tagLabel">{tbody.tag}</label>}</td>
-                <td className="dateCol NumberFont">{tbody.date}</td>
-                <td className={tbody.item === 1 ? "amountCol NumberFont amountCol-red " : "amountCol NumberFont amountCol-green"}>{tbody.expense}</td>
+              <tr key={tbody.id} className={tbody.transfer ? "record-trLine record-trLine-transfer" : "record-trLine"}>
+                <td className="record-itemCol">{item(tbody.item)}</td>
+                <td className="record-sortCol">{tbody.sort}</td>
+                <td className="record-wayCol">{tbody.way}</td>
+                <td className="record-accountCol">{tbody.account}</td>
+                <td className="record-descriptionCol">{tbody.description}</td>
+                <td className="record-tagCol">{tbody.tag === null ? '' : <label className="record-tagLabel">{tbody.tag}</label>}</td>
+                <td className="record-dateCol record-NumberFont">{tbody.date}</td>
+                <td className={tbody.item === 1 ? "record-amountCol record-NumberFont record-amountCol-red " : "record-amountCol record-NumberFont record-amountCol-green"}>{tbody.expense}</td>
               </tr>
             )}
           </tbody>
         </Table>
       </div>
-      <div className="totalSpan">
+      <div className="record-totalSpan">
         <span >
           當月結餘：
         </span>
-        <span className={count < 0 ? " NumberFont amountCol-red " : " NumberFont amountCol-green"}>{count}</span>
+        <span className={count < 0 ? " record-NumberFont record-amountCol-red " : " record-NumberFont record-amountCol-green"}>{count}</span>
       </div>
     </>
 
