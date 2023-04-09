3827b52e68230f09c3aa4c6273e9895dd9fb33de
Cathy <cathy0852753@gmail.com>
2023-04-10 00:19:25 +0800

    Add table menu, modify chart

--- src/page/Bank.js
+++ src/page/Bank.js
@@ -6,18 +6,26 @@ import { Form, Table, Row, Col, Container, } from "react-bootstrap"
 import { TabClick } from "../components/BodyNav"
 import '../components/css/BodyNav.css'
 import '../components/css/BankTable.css'
-import { tableHead, tableBody, account, sortE, sortI } from "../components/data"
+import { tableHead, tableBody, account, } from "../components/data"
 import Expenses from '../icon/expenses.png'
 import Income from '../icon/income.png'
 import "chart.js/auto"
-import { Pie } from "react-chartjs-2"
+import { Bar } from "react-chartjs-2"
+
+const MonthArray = ['一月', '二月', '三月', '四月', '五月', '六月', '七月', '八月', '九月', '十月', '十一月', '十二月']
 
 // 銀行選擇
 function SelectBank (params) {
   const [bank, setBank] = useState('現金')
+  const [month, setMonth] = useState('選擇月份')
   const changeBank = (e) => {
     // console.log('Bank', e)
     setBank(e.target.value)
+    setMonth('選擇月份')
+  }
+  const changeMonth = (e) => {
+    // console.log('month', e.target.value)
+    setMonth(e.target.value)
   }
   let bankBilling = [] // a 篩選銀行暫存的陣列
   for (let index = 0; index < tableBody.length; index++) {
@@ -40,19 +48,28 @@ function SelectBank (params) {
               <option key={el.value} id={el.value}>{el.label}</option>
             )}
           </Form.Select>
+          <Form.Select
+            className='bank-select bank-yearSelect'
+            onChange={changeMonth}
+            value={month}
+          >
+            <option>選擇月份</option>
+            {MonthArray.map(el =>
+              <option key={el} id={el}>{el}</option>
+            )}
+          </Form.Select>
         </div>
       </div>
       <Container className='bank-Container'>
         <Row className='bank-Row'>
           <Col xs={12} xl={7} className=''>
-            {BodyRecord(bankBilling)}
+            {BodyRecord(bankBilling, month)}
           </Col>
           <Col xs={12} xl={5} className=''>
             {Histogram(bankBilling)}
           </Col>
         </Row>
       </Container>
-
     </>
   )
 }
@@ -81,8 +98,7 @@ const ExpensesAndIncome = (type) => {
   }
 }
 
-
-function BodyRecord (bankBilling) {
+function BodyRecord (bankBilling, month) {
   // console.log(bankBilling)
   if (bankBilling.length === 0) {
     bankBilling = [{ id: null, item: null, transfer: null, sort: null, way: null, account: null, description: null, tag: null, date: null, expense: null, },]
@@ -95,7 +111,21 @@ function BodyRecord (bankBilling) {
       count = count + bankBilling[index].expense
     }
   }
-  // console.log(count)
+  // console.log(MonthArray.indexOf(month))
+  let monthBilling = []
+  if (MonthArray.indexOf(month) >= 0) {
+    for (let index = 0; index < bankBilling.length; index++) {
+      if (parseInt(bankBilling[index].date.substring(7, 5)) === (MonthArray.indexOf(month) + 1)) {
+        monthBilling.push({ id: bankBilling[index].id, item: bankBilling[index].item, transfer: bankBilling[index].transfer, sort: bankBilling[index].sort, way: bankBilling[index].way, account: bankBilling[index].account, description: bankBilling[index].description, tag: bankBilling[index].tag, date: bankBilling[index].date, expense: bankBilling[index].expense })
+      }
+    }
+    // console.log(456, monthBilling)
+  } else {
+    // console.log(123)
+    monthBilling = bankBilling
+    // console.log(123, monthBilling)
+  }
+
   return (
     <>
       <div className="bank-bg-table">
@@ -113,7 +143,7 @@ function BodyRecord (bankBilling) {
             )}
           </thead>
           <tbody>
-            {bankBilling.map(tbody =>
+            {monthBilling.map(tbody =>
               <tr key={tbody.id} className={tbody.item === 1 ? "bank-trLine bank-trLine-transfer" : "bank-trLine"}>
                 <td className="bank-itemCol">{ExpensesAndIncome(tbody.item)}</td>
                 <td className="bank-sortCol">{tbody.sort}</td>
@@ -128,7 +158,7 @@ function BodyRecord (bankBilling) {
       </div>
       <div className="bank-totalSpan">
         <span >
-          存款金額：
+          帳戶餘額：
         </span>
         <span className={count < 0 ? " bank-NumberFont bank-amountCol-red " : " bank-NumberFont bank-amountCol-green"}>{count}</span>
       </div>
@@ -137,82 +167,65 @@ function BodyRecord (bankBilling) {
 }
 
 function Histogram (bankBilling) {
-  const [select, setSelect] = useState(0)
-  const iStorColor = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-  ]
-  const iStorHoverColor = [
-    'rgba(255, 99, 132, 0.8)',
-    'rgba(54, 162, 235, 0.8)',
-    'rgba(255, 206, 86, 0.8)',
-    'rgba(75, 192, 192, 0.8)',
-    'rgba(153, 102, 255, 0.8)',
-    'rgba(255, 159, 64, 0.8)',
-  ]
-  const iSortBorderColor = [
-    'rgba(255, 99, 132, 1)',
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-  ]
-  const oStorColor = iStorColor.slice(0, iStorColor.length).reverse()
-  const oStorHoverColor = iStorHoverColor.slice(0, iStorHoverColor.length).reverse()
-  const oSortBorderColor = iSortBorderColor.slice(0, iSortBorderColor.length).reverse()
-  let sortMenu = []
-  let oSortMenu = [], iSortMenu = []
+  // const [select, setSelect] = useState(0)
+  // const iStorColor = [
+  //   'rgba(255, 99, 132, 0.2)',
+  //   'rgba(54, 162, 235, 0.2)',
+  //   'rgba(255, 206, 86, 0.2)',
+  //   'rgba(75, 192, 192, 0.2)',
+  //   'rgba(153, 102, 255, 0.2)',
+  //   'rgba(255, 159, 64, 0.2)',
+  // ]
+  // const iStorHoverColor = [
+  //   'rgba(255, 99, 132, 0.8)',
+  //   'rgba(54, 162, 235, 0.8)',
+  //   'rgba(255, 206, 86, 0.8)',
+  //   'rgba(75, 192, 192, 0.8)',
+  //   'rgba(153, 102, 255, 0.8)',
+  //   'rgba(255, 159, 64, 0.8)',
+  // ]
+  // const iSortBorderColor = [
+  //   'rgba(255, 99, 132, 1)',
+  //   'rgba(54, 162, 235, 1)',
+  //   'rgba(255, 206, 86, 1)',
+  //   'rgba(75, 192, 192, 1)',
+  //   'rgba(153, 102, 255, 1)',
+  //   'rgba(255, 159, 64, 1)',
+  // ]
+  // const oStorColor = iStorColor.slice(0, iStorColor.length).reverse()
+  // const oStorHoverColor = iStorHoverColor.slice(0, iStorHoverColor.length).reverse()
+  // const oSortBorderColor = iSortBorderColor.slice(0, iSortBorderColor.length).reverse()
+  let oSum = [], iSum = []
+  for (let index = 0; index < 12; index++) {
+    oSum[index] = 0
+    iSum[index] = 0
+  }
+
+  // console.log(parseInt(bankBilling[0].date.substring(7, 5)))
   for (let index = 0; index < bankBilling.length; index++) {
-    sortMenu.push(bankBilling[index].sort)
-  }
-  sortMenu = sortMenu.filter((item, index) => sortMenu.indexOf(item) === index)
-  for (let index = 0; index < sortMenu.length; index++) {
-    for (let sortEIndex = 0; sortEIndex < sortE.length; sortEIndex++) {
-      if (sortE[sortEIndex].label === sortMenu[index]) {
-        oSortMenu.push(sortMenu[index])
+    for (let monthIndex = 0; monthIndex < 12; monthIndex++) {
+      if (parseInt(bankBilling[index].date.substring(7, 5)) === (monthIndex + 1)) {
+        if (bankBilling[index].item === 1) {
+          oSum[monthIndex] += parseInt(bankBilling[index].expense)
+          // console.log(expense)
+        }
+        if (bankBilling[index].item === 0) {
+          iSum[monthIndex] += parseInt(bankBilling[index].expense)
+          // console.log(iSum[monthIndex])
+        }
       }
     }
-    for (let sortIIndex = 0; sortIIndex < sortI.length; sortIIndex++) {
-      if (sortI[sortIIndex].label === sortMenu[index]) {
-        iSortMenu.push(sortMenu[index])
-      }
-    }
   }
-  console.log(oSortMenu, iSortMenu)
-  // console.log(sortMenu)
-  // 每月的金額
-  let iSortSum = []
-  let oSortSum = []
-  let iSum = 0
-  let oSum = 0
-  for (let index = 0; index < oSortMenu.length; index++) {
-    for (let bill = 0; bill < bankBilling.length; bill++) {
-      if (bankBilling[bill].sort === oSortMenu[index]) {
-        oSum = oSum + parseInt(bankBilling[bill].expense)
-      }
-    }
-    oSortSum.push(oSum)
-  }
-  for (let index = 0; index < iSortMenu.length; index++) {
-    iSum = 0
-    for (let bill = 0; bill < bankBilling.length; bill++) {
-      if (bankBilling[bill].sort === iSortMenu[index]) {
-        iSum = iSum + parseInt(bankBilling[bill].expense)
-      }
-    }
-    iSortSum.push(iSum)
-  }
-  console.log(oSortSum, iSortSum)
-  const changeIO = (e) => {
-    console.log(e.target.options.selectedIndex)
-    setSelect(e.target.options.selectedIndex)
-  }
+  // console.log(oSum, iSum)
+
   const options = {
+    indexAxis: 'x',
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
     plugins: {
       legend: {
         position: 'bottom',
@@ -223,49 +236,39 @@ function Histogram (bankBilling) {
       },
     },
   }
-  const chartDataI = {
-    labels: iSortMenu,
+
+  const labels = MonthArray
+
+  const data = {
+    labels,
     datasets: [
       {
-        label: "金額",
-        backgroundColor: iStorColor,
-        hoverBackgroundColor: iStorHoverColor,
-        borderColor: iSortBorderColor,
+        label: '收入',
+        data: iSum,
+        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+        hoverBackgroundColor: 'rgba(75, 192, 192, 0.8)',
+        borderColor: 'rgba(75, 192, 192, 1)',
         borderWidth: 1,
-        data: iSortSum,
         offset: 10,
         hoverOffset: 15
-      }
-    ]
-  }
-  const chartDataO = {
-    labels: oSortMenu,
-    datasets: [
+      },
       {
-        label: "金額",
-        backgroundColor: oStorColor,
-        hoverBackgroundColor: oStorHoverColor,
-        borderColor: oSortBorderColor,
+        label: '支出',
+        data: oSum,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        hoverBackgroundColor: 'rgba(255, 99, 132, 0.8)',
+        borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
-        data: oSortSum,
         offset: 10,
         hoverOffset: 15
-      }
-    ]
+      },
+    ],
   }
   return (
     <>
-      <Form.Select
-        className='bank-select bank-yearSelect'
-        onChange={changeIO}
-      >
-        <option key='0' id='0'>收入</option>
-        <option key='1' id='1'>支出</option>
-      </Form.Select>
       <div className='bank-histogram'>
-        <Pie className='bank-Chart'
-
-          data={select === 0 ? chartDataI : chartDataO}
+        <Bar className='bank-Chart'
+          data={data}
           options={options} />
       </div>
     </>
--- src/page/Record.js
+++ src/page/Record.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'semantic-ui-css/semantic.min.css'
 import { AddBtn } from '../components/AddModal'
@@ -13,109 +13,116 @@ import Income from '../icon/income.png'
 import { Popover, Whisper, IconButton, Loader } from 'rsuite'
 import TagIcon from '@rsuite/icons/Tag'
 import "chart.js/auto"
-import { Chart } from "react-chartjs-2"
+import { Pie } from "react-chartjs-2"
 
 // Month Select
 
 const localDate = new Date()
-class SelectMonth extends React.Component {
-  state = {
-    years: localDate.getFullYear(),
-    month: localDate.getMonth() + 1
-  }
-  changeYear = (e) => {
+function SelectMonth (params) {
+  const [years, setYears] = useState(localDate.getFullYear())
+  const [month, setMonth] = useState(localDate.getMonth() + 1)
+  const [IO, setIO] = useState('全部')
+  const changeYear = (e) => {
     // console.log('Year', e)
-    this.setState({
-      years: e.target.value,
-    })
+    setYears(e.target.value)
+    setIO('全部')
   }
-  changeMonth = (e) => {
+  const changeMonth = (e) => {
     // console.log('Month', e)
-    this.setState({
-      month: e.target.value,
-    })
+    setMonth(e.target.value)
+    setIO('全部')
   }
-  render () {
-    //月份選單預設
-    let defaultM
-    if (this.state.month < 10) {
-      defaultM = '0' + this.state.month
+  const changeIO = (e) => {
+    setIO(e.target.value)
+  }
+
+  //月份選單預設
+  let defaultM
+  if (month < 10) {
+    defaultM = '0' + month
+  } else {
+    defaultM = month
+  }
+  // console.log(defaultM, this.state.month)
+
+  /* -----篩選年月資料存到a陣列------------------ */
+  let ab = years + '-' + defaultM
+  // console.log(ab, 'aaaa:' + ab.substring(7, 5))
+  if (ab.substring(7, 5) === '00') {
+    ab = years + '-' + ab.substring(8, 6)
+  }
+  // console.log('ab:' + ab)
+  let monthlyBilling = [] // monthlyBilling 篩選月份暫存的陣列
+  for (let index = 0; index < tableBody.length; index++) {
+    let d = tableBody[index].date
+    if (d.substring(0, 7) === ab) {
+      monthlyBilling.push({ id: tableBody[index].id, item: tableBody[index].item, transfer: tableBody[index].transfer, sort: tableBody[index].sort, way: tableBody[index].way, account: tableBody[index].account, description: tableBody[index].description, tag: tableBody[index].tag, date: tableBody[index].date, expense: tableBody[index].expense })
+    }
+  }
+  /* -----年份及月分選單------------------------ */
+  let yearSelect = []
+  let monthSelect = []
+  for (let yearsIndex = localDate.getFullYear() - 4; yearsIndex < localDate.getFullYear() + 2; yearsIndex++) {
+    yearSelect.push({ value: yearsIndex, label: yearsIndex })
+  }
+  for (let monthIndex = 1; monthIndex <= 12; monthIndex++) {
+    if (monthIndex < 10) {
+      monthSelect.push({ value: '0' + monthIndex, label: '0' + monthIndex })
     } else {
-      defaultM = this.state.month
+      monthSelect.push({ value: monthIndex, label: monthIndex })
     }
-    // console.log(defaultM, this.state.month)
-
-    /* -----篩選年月資料存到a陣列------------------ */
-    let ab = this.state.years + '-' + defaultM
-    // console.log(ab, 'aaaa:' + ab.substring(7, 5))
-    if (ab.substring(7, 5) === '00') {
-      ab = this.state.years + '-' + ab.substring(8, 6)
-    }
-    // console.log('ab:' + ab)
-    let monthlyBilling = [] // monthlyBilling 篩選月份暫存的陣列
-    for (let index = 0; index < tableBody.length; index++) {
-      let d = tableBody[index].date
-      if (d.substring(0, 7) === ab) {
-        monthlyBilling.push({ id: tableBody[index].id, item: tableBody[index].item, transfer: tableBody[index].transfer, sort: tableBody[index].sort, way: tableBody[index].way, account: tableBody[index].account, description: tableBody[index].description, tag: tableBody[index].tag, date: tableBody[index].date, expense: tableBody[index].expense })
-      }
-    }
-    // console.log('y:', this.state.years, ',m:', this.state.month) // y:目前年份 ,m: 目前月份
-    /* -----年份及月分選單------------------------ */
-    let yearSelect = []
-    let monthSelect = []
-    for (let years = localDate.getFullYear() - 4; years < localDate.getFullYear() + 2; years++) {
-      yearSelect.push({ value: years, label: years })
-    }
-    for (let month = 1; month <= 12; month++) {
-      if (month < 10) {
-        monthSelect.push({ value: '0' + month, label: '0' + month })
-      } else {
-        monthSelect.push({ value: month, label: month })
-      }
-    }
-    /* ----------------------------------------- */
-    return (
-      <>
-        <TabClick />
-        <div className='container-fluid'>
-          <div className='row record-itemHome'>
-            <Form.Select
-              className='record-select record-yearSelect'
-              onChange={this.changeYear}
-              defaultValue={this.state.years}
-            >
-              {yearSelect.map(el =>
-                <option key={el.value} id={el.value}>{el.label}</option>
-              )}
-            </Form.Select>
-            <span className='record-dateSpan'>-</span>
-            <Form.Select
-              className='record-select record-monthSelect'
-              onChange={this.changeMonth}
-              defaultValue={defaultM}
-            >
-              {monthSelect.map(el =>
-                <option key={el.value} id={el.value}>{el.label}</option>
-              )}
-            </Form.Select>
-            <AddBtn />
-          </div>
-        </div>
-        <Container className='record-Container'>
-          <Row xs={1} xl={2} className="record-Row">
-            <Col xs={12} xl={8} lg={12} className=''>
-              {BodyRecord(monthlyBilling)}
-            </Col>
-            <Col xs={12} xl={4} lg={12} className=''>
-              {Histogram(monthlyBilling)}
-            </Col>
-          </Row>
-        </Container>
-
-      </>
-    )
   }
+  /* ----------------------------------------- */
+  return (
+    <>
+      <TabClick />
+      <div className='container-fluid'>
+        <div className='row record-itemHome'>
+          <Form.Select
+            className='record-select record-yearSelect'
+            onChange={changeYear}
+            defaultValue={years}
+          >
+            {yearSelect.map(el =>
+              <option key={el.value} id={el.value}>{el.label}</option>
+            )}
+          </Form.Select>
+          <span className='record-dateSpan'>-</span>
+          <Form.Select
+            className='record-select record-monthSelect'
+            onChange={changeMonth}
+            defaultValue={defaultM}
+          >
+            {monthSelect.map(el =>
+              <option key={el.value} id={el.value}>{el.label}</option>
+            )}
+          </Form.Select>
+          <Form.Select
+            className='record-select record-IOSelect'
+            onChange={changeIO}
+            value={IO}
+          >
+            <option key='全部' id='全部'>全部</option>
+            <option key='收入' id='收入'>收入</option>
+            <option key='支出' id='支出'>支出</option>
+          </Form.Select>
+          <AddBtn />
+        </div>
+      </div>
+      <Container className='record-Container'>
+        <Row xs={1} xl={2} className="record-Row">
+          <Col xs={12} xl={8} lg={12} className=''>
+            {BodyRecord(monthlyBilling, IO)}
+          </Col>
+          <Col xs={12} xl={4} lg={12} className=''>
+            {Histogram(monthlyBilling, IO)}
+          </Col>
+        </Row>
+      </Container>
+    </>
+  )
 }
+
 
 const ExpensesAndIncome = (type) => {
   if (type === 1) {
@@ -141,7 +148,7 @@ const ExpensesAndIncome = (type) => {
   }
 }
 
-
+/*--標籤------------------------------------------------*/
 const DefaultPopover = React.forwardRef(({ content, ...props }, ref) => {
   const [loading, setLoading] = React.useState(true)
   React.useEffect(() => {
@@ -160,18 +167,28 @@ const DefaultPopover = React.forwardRef(({ content, ...props }, ref) => {
   )
 })
 
-const ShowTag = ({ placement, tag }) => (
-  <Whisper
-    trigger="click"
-    placement={placement}
-    controlId={`control-id-${placement}`}
-    speaker={<DefaultPopover content={tag} />}
-  >
-    <IconButton icon={<TagIcon />} appearance="subtle" />
-  </Whisper>
-)
+const ShowTag = ({ placement, tag }) => {
 
-function BodyRecord (monthlyBilling) {
+  return (
+    <>
+      {tag == null ? (
+        <></>
+      ) : (
+        <Whisper
+          trigger="click"
+          placement={placement}
+          controlId={`control-id-${placement}`}
+          speaker={<DefaultPopover content={tag} />}
+        >
+          <IconButton icon={<TagIcon />} appearance="subtle" />
+        </Whisper>
+      )}
+    </>
+  )
+}
+/*------------------------------------------------------*/
+
+function BodyRecord (monthlyBilling, IO) {
   // console.log(monthlyBilling)
   if (monthlyBilling.length === 0) {
     monthlyBilling = [{ id: null, item: null, transfer: null, sort: null, way: null, account: null, description: null, tag: null, date: null, expense: null, },]
@@ -184,7 +201,27 @@ function BodyRecord (monthlyBilling) {
       count = count + monthlyBilling[index].expense
     }
   }
-  // console.log(count)
+  // console.log(monthlyBilling)
+  let IOBilling = []
+  if (IO === '全部') {
+    IOBilling = monthlyBilling
+    // console.log(123, IOBilling)
+
+  } else if (IO === '收入') {
+    for (let index = 0; index < monthlyBilling.length; index++) {
+      if (monthlyBilling[index].item === 0) {
+        IOBilling.push({ id: monthlyBilling[index].id, item: monthlyBilling[index].item, transfer: monthlyBilling[index].transfer, sort: monthlyBilling[index].sort, way: monthlyBilling[index].way, account: monthlyBilling[index].account, description: monthlyBilling[index].description, tag: monthlyBilling[index].tag, date: monthlyBilling[index].date, expense: monthlyBilling[index].expense })
+      }
+    }
+    // console.log(456, IOBilling)
+  } else if (IO === '支出') {
+    for (let index = 0; index < monthlyBilling.length; index++) {
+      if (monthlyBilling[index].item === 1) {
+        IOBilling.push({ id: monthlyBilling[index].id, item: monthlyBilling[index].item, transfer: monthlyBilling[index].transfer, sort: monthlyBilling[index].sort, way: monthlyBilling[index].way, account: monthlyBilling[index].account, description: monthlyBilling[index].description, tag: monthlyBilling[index].tag, date: monthlyBilling[index].date, expense: monthlyBilling[index].expense })
+      }
+    }
+    // console.log(789, IOBilling)
+  }
 
   return (
     <>
@@ -205,7 +242,7 @@ function BodyRecord (monthlyBilling) {
             )}
           </thead>
           <tbody>
-            {monthlyBilling.map(tbody =>
+            {IOBilling.map(tbody =>
               <tr key={tbody.id} className={tbody.item === 1 ? "record-trLine record-trLine-transfer" : "record-trLine"}>
                 <td className="record-itemCol">{ExpensesAndIncome(tbody.item)}</td>
                 <td className="record-sortCol">{tbody.sort}</td>
@@ -230,57 +267,142 @@ function BodyRecord (monthlyBilling) {
   )
 }
 
-const Histogram = (monthlyBilling) => {
-  let d = []
-  for (let index = 0; index < monthlyBilling.length; index++) {
-    d.push(monthlyBilling[index].sort)
-  }
-  let b = d.filter((item, index) => d.indexOf(item) === index)
-  // console.log(d, b)
-  // 每月的金額
-  let c = []
-  for (let index = 0; index < b.length; index++) {
-    // console.log(b[index])
-    let mTotal = 0
-    for (let tbodyIndex = 0; tbodyIndex < monthlyBilling.length; tbodyIndex++) {
-      // console.log(monthlyBilling[tbodyIndex].sort, monthlyBilling[tbodyIndex].expense)
-      if (monthlyBilling[tbodyIndex].sort === b[index]) {
-        mTotal = mTotal + parseInt(monthlyBilling[tbodyIndex].expense, 0)
+function Histogram (monthlyBilling, IO) {
+  const iStorColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(255, 206, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+  ]
+  const iStorHoverColor = [
+    'rgba(255, 99, 132, 0.8)',
+    'rgba(54, 162, 235, 0.8)',
+    'rgba(255, 206, 86, 0.8)',
+    'rgba(75, 192, 192, 0.8)',
+    'rgba(153, 102, 255, 0.8)',
+    'rgba(255, 159, 64, 0.8)',
+  ]
+  const iSortBorderColor = [
+    'rgba(255, 99, 132, 1)',
+    'rgba(54, 162, 235, 1)',
+    'rgba(255, 206, 86, 1)',
+    'rgba(75, 192, 192, 1)',
+    'rgba(153, 102, 255, 1)',
+    'rgba(255, 159, 64, 1)',
+  ]
+  const oStorColor = iStorColor.slice(0, iStorColor.length).reverse()
+  const oStorHoverColor = iStorHoverColor.slice(0, iStorHoverColor.length).reverse()
+  const oSortBorderColor = iSortBorderColor.slice(0, iSortBorderColor.length).reverse()
+
+  let SortMenu = []
+  let SortSum = []
+  let Sum
+  // console.log(monthlyBilling)
+  if (IO === '全部') {
+    SortMenu = ['收入', '支出']
+    for (let index = 0; index < SortMenu.length; index++) {
+      SortSum[index] = 0
+    }
+    for (let index = 0; index < monthlyBilling.length; index++) {
+      if (monthlyBilling[index].item === 0) {
+        SortSum[0] += parseInt(monthlyBilling[index].expense)
+      }
+      if (monthlyBilling[index].item === 1) {
+        SortSum[1] += parseInt(monthlyBilling[index].expense)
       }
     }
-    c.push(mTotal)
-  }
-  // 正負金額的顏色
-  let cBackgroundColor = []
-  let cHoverBackgroundColor = []
-  for (let index = 0; index < c.length; index++) {
-    if (c[index] >= 0) {
-      cBackgroundColor.push("rgb(130, 171, 163, 0.5)")
-      cHoverBackgroundColor.push("rgb(130,171,163,1)")
-    } else {
-      cBackgroundColor.push("rgb(185,87,86,0.5)")
-      cHoverBackgroundColor.push("rgb(85,87,86,1)")
+  } else if (IO === '收入') {
+    for (let sortIndex = 0; sortIndex < monthlyBilling.length; sortIndex++) {
+      // console.log(monthlyBilling[sortIndex].sort, monthlyBilling[sortIndex].item)
+      if (monthlyBilling[sortIndex].item === 0) {
+        SortMenu.push(monthlyBilling[sortIndex].sort)
+      }
     }
-
+    SortMenu = SortMenu.filter((item, index) => SortMenu.indexOf(item) === index)
+    for (let index = 0; index < SortMenu.length; index++) {
+      Sum = 0
+      for (let bill = 0; bill < monthlyBilling.length; bill++) {
+        if (monthlyBilling[bill].sort === SortMenu[index]) {
+          Sum = Sum + parseInt(monthlyBilling[bill].expense)
+        }
+      }
+      SortSum.push(Sum)
+    }
+  } else if (IO === '支出') {
+    for (let sortIndex = 0; sortIndex < monthlyBilling.length; sortIndex++) {
+      // console.log(monthlyBilling[sortIndex].sort, monthlyBilling[sortIndex].item)
+      if (monthlyBilling[sortIndex].item === 1) {
+        SortMenu.push(monthlyBilling[sortIndex].sort)
+      }
+    }
+    SortMenu = SortMenu.filter((item, index) => SortMenu.indexOf(item) === index)
+    for (let index = 0; index < SortMenu.length; index++) {
+      Sum = 0
+      for (let bill = 0; bill < monthlyBilling.length; bill++) {
+        if (monthlyBilling[bill].sort === SortMenu[index]) {
+          Sum = Sum + parseInt(monthlyBilling[bill].expense)
+        }
+      }
+      SortSum.push(Sum)
+    }
   }
-  // console.log('c:', c)
-  const chartData = {
-    labels: b,
+  // console.log(SortMenu, SortSum)
+  const options = {
+    plugins: {
+      legend: {
+        position: 'bottom',
+      },
+      title: {
+        display: true,
+        text: IO,
+      },
+    },
+  }
+  const chartDataIO = {
+    labels: ['收入', '支出'],
     datasets: [
       {
         label: "金額",
-        backgroundColor: cBackgroundColor,
-        hoverBackgroundColor: cHoverBackgroundColor,
-        data: c
+        backgroundColor: ['rgba(75, 192, 192, 0.2)', 'rgba(255, 99, 132, 0.2)'],
+        hoverBackgroundColor: ['rgba(75, 192, 192, 0.8)', 'rgba(255, 99, 132, 0.8)'],
+        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+        data: SortSum,
+        offset: 10,
+        hoverOffset: 15
+      }
+    ]
+  }
+  const chartData = {
+    labels: SortMenu,
+    datasets: [
+      {
+        label: "金額",
+        backgroundColor: IO === '收入' ? iStorColor : oStorColor,
+        hoverBackgroundColor: IO === '收入' ? iStorHoverColor : oStorHoverColor,
+        borderColor: IO === '收入' ? iSortBorderColor : oSortBorderColor,
+        borderWidth: 1,
+        data: SortSum,
+        offset: 10,
+        hoverOffset: 15
       }
     ]
   }
   return (
-    <div className='record-histogram'>
-      <Chart type="doughnut" data={chartData} options={""} />
-    </div>
+    <>
+      <div className='bank-histogram'>
+        <Pie className='bank-Chart'
+
+          data={IO === '全部' ? chartDataIO : chartData}
+          options={options} />
+      </div>
+    </>
+
   )
 }
+
 
 export default function Record () {
   return (
